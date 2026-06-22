2fcbcbb9e783b9a452bbc23e5d83b17b77801c3d
bsrarellano-beep <bsr.arellano@yavirac.edu.ec>
2026-06-22 01:46:39 -0500

    Login auth jwt(1)

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterAuthDto } from './dto/register-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    @Post('register')
-    registerUser(@Body()userObj : RegisterAuthDto){
-        userObj.name;
-        userObj.email;
-        userObj.password
 
+    constructor(private authService: AuthService){        
+    }
+    @Post('register')
+    registerUser(@Body()userObj : RegisterAuthDto) {
+        console.log(userObj);
+        return userObj;
+    }
+    @Post('login')
+    login(@Body() credenciales: LoginAuthDto){
+        return this.authService.login(credenciales)
     }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports:[
+        JwtModule.register({
+            secret: "MI CODIGO SECRETO",
+            signOptions:{expiresIn: '60S'}
+        })
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
